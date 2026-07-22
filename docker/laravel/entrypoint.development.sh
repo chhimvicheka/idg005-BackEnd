1832769b88aab6ca337ec09c53ddfb82cbe898f5
chhimvicheka <chhim.vicheka168@gmail.com>
2026-07-22 09:51:40 +0700

    Session 5.2

--- docker/laravel/entrypoint.development.sh
+++ docker/laravel/entrypoint.development.sh
@@ -4,9 +4,11 @@ set -e
 # composer install
 # wait $!
 # npm install
-# wait $!
+wait $!
 php artisan key:generate
 wait $!
 php artisan migrate
+wait $!
+php artisan storage:link
 wait $!
 exec supervisord -c /etc/supervisor/conf.d/supervisord.development.conf
