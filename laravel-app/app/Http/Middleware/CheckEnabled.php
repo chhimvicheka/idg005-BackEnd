b4532788c63fcba17809bb0fba07a1ccdafdcb49
chhimvicheka <chhim.vicheka168@gmail.com>
2026-07-29 15:15:50 +0700

    Session 8.4

--- laravel-app/app/Http/Middleware/CheckEnabled.php
+++ laravel-app/app/Http/Middleware/CheckEnabled.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class CheckEnabledMiddleware
+class CheckEnabled
 {
     /**
      * Handle an incoming request.
